889dcd998fc8f2be8915ab89b664803ca97dba62
dgp52 <deeppatel19940@gmail.com>
2016-03-04 19:07:48 -0500

    Knight implemented

--- Chess/src/pieces/Knight.java
+++ Chess/src/pieces/Knight.java
@@ -24,8 +24,21 @@ public class Knight extends AbstractPiece {
 
 	@Override
 	public boolean isMoveAllowed(Point beg, Point end, Board b) {
-		// TODO Auto-generated method stub
-		return false;
+		if (beg.x == end.getX() || beg.getY() == end.getY()) {
+			System.out.println("Knight moving straight! no no");
+			return false;
+		} else if ((end.getX()+1==beg.getX() || end.getX()-1==beg.getX()) && 
+				(end.getY()-2==beg.getY() ||end.getY()+2==beg.getY())){
+			System.out.println("Knight 1 up +2 -2 left right");
+			return true;
+		} else if ((end.getX()+2==beg.getX() || end.getX()-2==beg.getX()) &&
+				(end.getY()-1==beg.getY() ||end.getY()-1==beg.getY())){
+			System.out.println("Knight 2 up +1 -1 left right");
+			return true;
+		} else {
+			System.out.println("Knight failed");
+			return false;
+		}
 	}
 
 }
